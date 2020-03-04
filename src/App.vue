38e49499bafd32fe0599a5b7961bd53ce9694553
aaronreulkhoo <aaronkhoo@live.com>
2020-03-04 18:47:14 +0800

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <v-content>
-      <router-view/>
+      <router-view>
+      </router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import router from './router';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
+  vuetify,router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+
+
