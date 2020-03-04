81edb263924790eb7c0bf5846986ae663d3bdecb
aaronreulkhoo <aaronkhoo@live.com>
2020-03-04 17:24:12 +0800

    added vuetify

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h1>About PAge</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
