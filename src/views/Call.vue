3311a769500a231bbf994c55772bc6f2faeeb4ad
jeroee <jeroejeroe@gmail.com>
2020-03-20 17:30:04 +0800

    basic call

--- src/views/Call.vue
+++ src/views/Call.vue
@@ -9,10 +9,6 @@
              <v-btn color="red lighten-2" dark v-on="on"> Start Call </v-btn>
           </div>  
         </template>
-
-        
-        
-  
         <v-card>
           <v-card-title class="headline grey lighten-2" primary-title> Customer Name</v-card-title>
           <v-card-text  class="headline grey lighten-2" process> Calling .....</v-card-text>
@@ -102,9 +98,8 @@
 </template>
 
 <script>
-<script>
 import rainbowSDK from "rainbow-web-sdk";
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   name: "Call",
@@ -169,35 +164,37 @@ export default {
           console.log(error);
         });
 
-      let response = await axios.get(
-        `http://still-sea-41149.herokuapp.com/api/agentss?category=${this.selectedIndex}` //obtain agent through category
-      );
-      let agent_id = response.data.agent.rainbowId; //get agent id
-      let agent_name = response.data.agent.name; //get agent name
-      let token = response.data.token; //get guest token
-      // let agent_id = "5e4950b6e9f12730636972b5";
-      // let token =
-      //   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3VudFJlbmV3ZWQiOjAsIm1heFRva2VuUmVuZXciOjcsInVzZXIiOnsiaWQiOiI1ZTc0N2JmMGY0M2ZjMzZhYjBmM2I1NzIiLCJsb2dpbkVtYWlsIjoiMjRubTdqdzZxNXBxN3BnNGNuNzd4eWU4NzU1YjB4bmxkYWFtMzg3Y0BhNThjZmFjMDViMDcxMWVhYmY3ZTc3ZDE0ZTg3YjkzNi5zYW5kYm94Lm9wZW5yYWluYm93LmNvbSJ9LCJhcHAiOnsiaWQiOiJhNThjZmFjMDViMDcxMWVhYmY3ZTc3ZDE0ZTg3YjkzNiIsIm5hbWUiOiJhY29ybi1iYWNrZW5kIn0sImlhdCI6MTU4NDY5MjIwOCwiZXhwIjoxNTg1OTg4MjA4fQ.t8myU5bOjL1RSDT5FKO4cr8k6CihLQCEyxjSFHfbYPIx2dOZcQxcN4to4EFNFSpnKryc9YmMImLbi7nKpLUQt-RN7NTCH33YtjH5mQbisxE3K0uSgCBUU2gHbRaH-4z7NTW-1TPuEc1AVATWxTOUuiubntJdl4OfsHrHvFi3paWhIk4JGBFIjSCbSNSeJkrqKGpoLuATZ5pEOZwfi4YZi-MQQsCX_5V2ys5tsWl7fxO4Bpn3OaF_7HobIzfsJA8wxC7zfkcMtMZwaL4_YlbY7_NXdd5QXoAPT7cKIYpZEbBHwYGDWHZ9p1RbcdhLEC-WfpYre3gHEKu81nJ7GpPZKA";
-      console.log("agent ID is: ", agent_id);
-      console.log("agent name is: ", agent_name);
-      console.log("token is: ", token);
+      // let response = await axios.get(
+      //   `http://still-sea-41149.herokuapp.com/api/agentss?category=${this.selectedIndex}` //obtain agent through category
+      // );
+      // let agent_id = response.data.agent.rainbowId; //get agent id
+      // let agent_name = response.data.agent.name; //get agent name
+      // let token = response.data.token; //get guest token
+      let agent_id = "5e4950b6e9f12730636972b5";
+       let token =
+         "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3VudFJlbmV3ZWQiOjAsIm1heFRva2VuUmVuZXciOjcsInVzZXIiOnsiaWQiOiI1ZTc0N2JmMGY0M2ZjMzZhYjBmM2I1NzIiLCJsb2dpbkVtYWlsIjoiMjRubTdqdzZxNXBxN3BnNGNuNzd4eWU4NzU1YjB4bmxkYWFtMzg3Y0BhNThjZmFjMDViMDcxMWVhYmY3ZTc3ZDE0ZTg3YjkzNi5zYW5kYm94Lm9wZW5yYWluYm93LmNvbSJ9LCJhcHAiOnsiaWQiOiJhNThjZmFjMDViMDcxMWVhYmY3ZTc3ZDE0ZTg3YjkzNiIsIm5hbWUiOiJhY29ybi1iYWNrZW5kIn0sImlhdCI6MTU4NDY5MjIwOCwiZXhwIjoxNTg1OTg4MjA4fQ.t8myU5bOjL1RSDT5FKO4cr8k6CihLQCEyxjSFHfbYPIx2dOZcQxcN4to4EFNFSpnKryc9YmMImLbi7nKpLUQt-RN7NTCH33YtjH5mQbisxE3K0uSgCBUU2gHbRaH-4z7NTW-1TPuEc1AVATWxTOUuiubntJdl4OfsHrHvFi3paWhIk4JGBFIjSCbSNSeJkrqKGpoLuATZ5pEOZwfi4YZi-MQQsCX_5V2ys5tsWl7fxO4Bpn3OaF_7HobIzfsJA8wxC7zfkcMtMZwaL4_YlbY7_NXdd5QXoAPT7cKIYpZEbBHwYGDWHZ9p1RbcdhLEC-WfpYre3gHEKu81nJ7GpPZKA";
+      // console.log("agent ID is: ", agent_id);
+      // console.log("agent name is: ", agent_name);
+      // console.log("token is: ", token);
       let account = await rainbowSDK.connection.signinSandBoxWithToken(token); //login to rainbow server with guest token
       if (account) {
         console.log("sign in success");
         this.contact = await rainbowSDK.contacts.searchById(agent_id); //get contact from agent id
         console.log(this.contact);
-        // var call = rainbowSDK.webRTC.callInAudio(this.contact);
-        // if (call.label === "OK") {
-        //   console.log("your call has been correctly initialised");
-        // }
+        //after this line the loading page will disappear showing us the calling page
+        this.start=false;
+        var call = rainbowSDK.webRTC.callInAudio(this.contact);
+        if (call.label === "OK") {
+          console.log("your call has been correctly initialised");
+        }
       }
     },
-    calling: function() {
-      var call = rainbowSDK.webRTC.callInAudio(this.contact);
-      if (call.label === "OK") {
-        console.log("your call has been correctly initialised");
-      }
-    }
+    // calling: function() {
+    //   var call = rainbowSDK.webRTC.callInAudio(this.contact);
+    //   if (call.label === "OK") {
+    //     console.log("your call has been correctly initialised");
+    //   }
+    // }
   }
 };
 
