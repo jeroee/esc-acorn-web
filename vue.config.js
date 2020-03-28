3fb5047915870af58afda3422413c588be6a4dd0
aaronreulkhoo <aaronkhoo@live.com>
2020-03-28 12:59:48 +0800

    fixes, call integration, store connection

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   devServer: {
+    host: 'localhost',
+    port: 8080,
     https: {
       key: fs.readFileSync('key.pem'),
       cert: fs.readFileSync('certificate.pem'),
     },
-    public: 'https://localhost:8080/'
-}
-}
+  }
+};
