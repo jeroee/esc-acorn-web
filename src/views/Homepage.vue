c064227f830c00fd46bc3dcabeaedd88d92d37c8
chenyan1998 <49365834+chenyan1998@users.noreply.github.com>
2020-04-06 20:56:49 +0800

    CheckAvalibilityOfChatAndCall
    Add Name in the home page and check 'Call' and 'Chat' Avability

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -12,20 +12,35 @@
     <br />
     <br />
     <v-container class="flex-col">
-      <v-text-field
-        v-model="firstName"
+
+       <v-text-field 
+        id="FirstName" 
+        v-model="name1"
         style="width: 400px"
         color="green"
         outlined="true"
+        :error-messages="name1Errors"
+        :counter="30"
         label="Insert your first name"
-      ></v-text-field>
-      <v-text-field
-        v-model="lastName"
+        required
+        @input="$v.name1.$touch()"
+        @blur="$v.name1.$touch()"></v-text-field>
+
+
+      <v-text-field 
+        id="LastName" 
+        v-model="name2"
         style="width: 400px"
         color="green"
         outlined="true"
+        :error-messages="name2Errors"
+        :counter="30"
         label="Insert your last name"
-      ></v-text-field>
+        required
+        @input="$v.name2.$touch()"
+        @blur="$v.name2.$touch()"></v-text-field>
+
+      
 
       <v-select
         id="category"
@@ -37,13 +52,14 @@
         :menu-props="{ offsetY:true, openOnClick:false }"
         label="Choose a support category..."
       ></v-select>
+
       <div style="flex-direction: row">
         <v-btn
           @click="requestChat"
           x-large
           depressed
           class="ma-5 green white--text"
-          :disabled="isSelected"
+          :disabled="isSelected" 
         >
           <v-icon left>message</v-icon>Get Chat Support
         </v-btn>
@@ -53,10 +69,16 @@
           x-large
           depressed
           class="ma-5 green white--text"
-          :disabled="isSelected"
+          :disabled="isSelected" 
         >
           <v-icon left>phone</v-icon>Get Call Support
         </v-btn>
+
+        <v-col>
+        <v-btn class="mr-4"  @click="submit">submit2</v-btn>
+        <v-btn @click="clear">clear</v-btn>
+      </v-col>
+
       </div>
 
       <ManyCard></ManyCard>
@@ -66,59 +88,88 @@
 
 <script>
 // import popup from '../components/popup' // this is the popup table
+import { required, maxLength} from 'vuelidate/lib/validators'
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import { validationMixin } from 'vuelidate'
+Vue.use(Vuelidate)
 import Menu from "../components/Menu";
 import ManyCard from "../components/ManyCard";
 import FirstPagePic from "../components/FirstPagePic"; // this is the popup table
+
 
 export default {
   components: { FirstPagePic, Menu, ManyCard },
 
   name: "Home",
+  mixins: [validationMixin],
+
+  validations: {
+    name1: { required, maxLength: maxLength(30) },
+    name2: { required, maxLength: maxLength(30) },
+  },
+
+  
+
   data: () => ({
     categories: ["Acorn Products", "Acorn Services", "Acorn Applications"],
     selected: "",
     firstName: "",
-    lastName: ""
+    lastName: "",
+    name1:"",
+    name2:""
+    
   }),
   computed: {
     isSelected() {
-      return this.selected === "";
+      if(this.name2 =="" || this.name1 == "" || this.selected == ""){
+        return true
+      }
+      return false;
+    },
+    name1Errors() {
+      const errors = [];
+      if (!this.$v.name1.$dirty) return errors;
+      !this.$v.name1.maxLength && errors.push('Name must be at most 10 characters long');
+      !this.$v.name1.required && errors.push('Name is required.');
+      return errors;
+    },
+    name2Errors() {
+      const errors = [];
+      if (!this.$v.name2.$dirty) return errors;
+      !this.$v.name2.maxLength && errors.push('Name must be at most 30 characters long');
+      !this.$v.name2.required && errors.push('Name is required.');
+      return errors;
     }
+
   },
   methods: {
     alert: function() {
       alert("Coming Soon");
     },
-    requestChat: async function() {
-      if (this.selected && this.firstName != "" && this.lastName != "") {
-        this.$store.state.categoryIndex = this.categories.indexOf(
-          this.selected
-        );
-        this.$store.state.categoryName = this.selected;
-        this.$store.state.firstName = this.firstName;
-        this.$store.state.lastName = this.lastName;
-        this.$router.push({ path: "/chat" });
-      } else {
-        alert("Please key in valid Name");
-      }
+
+    submit() {
+    this.$v.$touch();
     },
-    // showAlert: function(){
-    //     const options = {title: 'Info', size: 'sm'}
-    //     dialog.alert('Your message', options)
-    //     .then(res => {
-    //     console.log(res) // {ok: true|false|undefined}
-    // })
-    requestCall: function() {
-      if (this.selected && this.firstName != "" && this.lastName != "") {
-        this.$store.state.categoryIndex = this.categories.indexOf(
-          this.selected
-        );
-        this.$store.state.categoryName = this.selected;
-        this.$router.push({ path: "/call" });
-      } else {
-        alert("Please key in valid Name");
-      }
-    }
+   
+
+        requestChat: function () {
+                  if (this.selected) {
+                      this.$store.state.categoryIndex = this.categories.indexOf(this.selected);
+                      this.$store.state.categoryName = this.selected;
+                      this.$router.push({path: "/chat"});
+                  }
+              },
+
+        requestCall: function () {
+            if (this.selected ) {
+                this.$store.state.categoryIndex = this.categories.indexOf(this.selected);
+                this.$store.state.categoryName = this.selected;
+                this.$router.push({path: "/call"});
+            }
+        },
+
+
   }
 };
 </script>
